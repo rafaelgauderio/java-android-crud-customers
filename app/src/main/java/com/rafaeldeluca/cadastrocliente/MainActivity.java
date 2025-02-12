b2750dbc8a3a2f5818667ba73e9adc50e4de58a0
Rafael de Luca <deluca1712@gmail.com>
2025-02-11 21:36:22 -0300

    action button clean

--- app/src/main/java/com/rafaeldeluca/cadastrocliente/MainActivity.java
+++ app/src/main/java/com/rafaeldeluca/cadastrocliente/MainActivity.java
@@ -1,10 +1,16 @@
 package com.rafaeldeluca.cadastrocliente;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName, editTextAverage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editTextName = findViewById((R.id.editTextNome));
+        editTextAverage = findViewById(R.id.editTextMedia);
     }
+
+    public void cleanfields(View view) {
+        editTextName.setText(null);
+        editTextAverage.setText(null);
+        editTextName.requestFocus();
+
+        //message
+        Toast.makeText(this, R.string.os_valores_de_nome_e_m_dia_foram_deletados, Toast.LENGTH_LONG).show();
+
+    }
+
 }
