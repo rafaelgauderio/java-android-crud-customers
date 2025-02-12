47f6dbb5dc42b83e9e9f70463c6a33685c1b028d
Rafael de Luca <deluca1712@gmail.com>
2025-02-12 12:06:16 -0300

    button save action

--- app/src/main/java/com/rafaeldeluca/cadastrocliente/MainActivity.java
+++ app/src/main/java/com/rafaeldeluca/cadastrocliente/MainActivity.java
@@ -30,4 +30,39 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.os_valores_de_nome_e_m_dia_foram_deletados, Toast.LENGTH_LONG).show();
     }
 
+    public void saveFiedlsValues(View view) {
+
+        String name = editTextName.getText().toString();
+        String nameWithoutSpace = name.trim();
+        String average = editTextAverage.getText().toString();
+
+        if (name == null || nameWithoutSpace.isEmpty() || nameWithoutSpace.isBlank()) {
+            Toast.makeText(this, R.string.campo_nome_preenchimento_obrigatorio, Toast.LENGTH_LONG).show();
+
+            editTextName.requestFocus();
+            return;
+        }
+        if (average == null || average.isEmpty() || average.isBlank()) {
+            Toast.makeText(this, "Campo média é de preenchumento obrigatório", Toast.LENGTH_LONG).show();
+            editTextAverage.requestFocus();
+            return;
+        }
+        int averageNumber = 0;
+
+        try {
+            averageNumber = Integer.parseInt(average);
+        } catch (NumberFormatException nfe) {
+            Toast.makeText(this, "A média dever ser um valor inteiro entre zero e 100", Toast.LENGTH_LONG).show();
+            editTextAverage.requestFocus();
+            editTextAverage.setSelection(0, average.length());
+            return;
+        }
+        StringBuilder finalToast = new StringBuilder();
+        finalToast.append(getString(R.string.nome_valor));
+        finalToast.append(name + "\n");
+        finalToast.append(R.string.media_valor);
+        finalToast.append(averageNumber);
+        // if everithing ok, show values on a Toast
+        Toast.makeText(this, finalToast, Toast.LENGTH_LONG).show();
+    }
 }
