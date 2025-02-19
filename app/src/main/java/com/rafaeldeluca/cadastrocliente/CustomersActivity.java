cb95fab74dc3f63d518473efc916f10a8ad59c1b
Rafael de Luca <deluca1712@gmail.com>
2025-02-19 17:08:23 -0300

    customers activity - list view data

--- app/src/main/java/com/rafaeldeluca/cadastrocliente/CustomersActivity.java
+++ app/src/main/java/com/rafaeldeluca/cadastrocliente/CustomersActivity.java
@@ -1,19 +1,68 @@
 package com.rafaeldeluca.cadastrocliente;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.rafaeldeluca.cadastrocliente.entities.Customer;
+import com.rafaeldeluca.cadastrocliente.entities.enums.Type;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomersActivity extends AppCompatActivity {
+
+    private ListView customersListView;
+    private List<Customer> customersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_customers);
+
+        customersListView = findViewById(R.id.listViewCustomers);
+
+        //insertData
+        insertCustomersListData();
+    }
+
+    private void insertCustomersListData() {
+
+        String[] customersBuyerName = getResources().getStringArray(R.array.customers_buyers_name);
+        String[] customersCorporateReason = getResources().getStringArray(R.array.customers_corporate_reason);
+        String[] customersEmail = getResources().getStringArray(R.array.customers_email);
+        int[] customersRestriction = getResources().getIntArray(R.array.customers_restriction);
+        int[] customersType = getResources().getIntArray(R.array.customers_type);
+        int[] customersDivision = getResources().getIntArray(R.array.customers_divison);
+
+        customersList = new ArrayList<Customer>();
+
+        Customer customer;
+        boolean restriction;
+        Type type;
+        Type[] valuesCustomersType = Type.values();
+
+        for (int index = 0; customersBuyerName.length > index; index++) {
+            if (customersRestriction[index] == 1) {
+                restriction = true;
+            } else {
+                restriction = false;
+            }
+            type = valuesCustomersType[customersType[index]];
+            customer = new Customer(customersBuyerName[index],
+                    customersCorporateReason[index],
+                    customersEmail[index],
+                    restriction, type, customersDivision[index]);
+
+            customersList.add(customer);
+        } // end for
+
+        ArrayAdapter<Customer> customerAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, customersList);
+        customersListView.setAdapter(customerAdapter);
     }
 }
