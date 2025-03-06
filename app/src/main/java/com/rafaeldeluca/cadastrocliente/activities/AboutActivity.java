65ed54bffca0960971039bce0c81e21cc39afa79
Rafael de Luca <deluca1712@gmail.com>
2025-03-06 15:24:49 -0300

    static activity about

---
+++ app/src/main/java/com/rafaeldeluca/cadastrocliente/activities/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.rafaeldeluca.cadastrocliente.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.rafaeldeluca.cadastrocliente.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
