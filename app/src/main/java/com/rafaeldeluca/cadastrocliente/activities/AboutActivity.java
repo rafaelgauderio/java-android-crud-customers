255519d5af3bcd8723089c0973bc7cd5f1ca5d69
Rafael de Luca <deluca1712@gmail.com>
2025-03-07 12:35:12 -0300

    opening sites programmatically using implicit intent

--- app/src/main/java/com/rafaeldeluca/cadastrocliente/activities/AboutActivity.java
+++ app/src/main/java/com/rafaeldeluca/cadastrocliente/activities/AboutActivity.java
@@ -1,6 +1,10 @@
 package com.rafaeldeluca.cadastrocliente.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +20,28 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
         this.setTitle(R.string.about);
     }
+
+    public void openSiteCourse (View view) {
+        openUrl("https://pos-graduacao-ead.cp.utfpr.edu.br/java/");
+    }
+
+    public void openSiteAuthor (View view) {
+    openUrl("https://github.com/rafaelgauderio");
+    }
+
+    private void openUrl(String stringUrl) {
+        Intent intentOpen = new Intent(Intent.ACTION_VIEW);
+        intentOpen.setData(Uri.parse(stringUrl));
+
+        if(intentOpen.resolveActivity(getPackageManager())!=null) {
+            startActivity(intentOpen);
+        } else {
+            Toast.makeText(this, getString(R.string.nenhum_aplicativo_instalado_para_abrir_paginas_web),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
